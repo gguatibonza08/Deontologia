aacc9bec762b0e1dfecabd976fa62877d193d93d
Gian Carlos Guatibonza Suarez <gguatibonza08@gmail.com>
2018-04-09 00:52:05 -0500

    Se creo la interfaz de acerca del juego y se inicio la interfaz de como jugar

---
+++ app/src/main/java/co/com/gguatibonza/deontologia/ComoJugar.java
@@ -0,0 +1,13 @@
+package co.com.gguatibonza.deontologia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComoJugar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_como_jugar);
+    }
+}

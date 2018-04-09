fb364eb1402e86014f5d7fce89110a763b95b2ba
Gian Carlos Guatibonza Suarez <gguatibonza08@gmail.com>
2018-04-09 00:21:17 -0500

    se agrego audio y botones para el inicio del juego

--- app/src/main/java/co/com/gguatibonza/deontologia/MainActivity.java
+++ app/src/main/java/co/com/gguatibonza/deontologia/MainActivity.java
@@ -1,13 +1,19 @@
 package co.com.gguatibonza.deontologia;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer md;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        md = MediaPlayer.create(this, R.raw.juego);
+        md.start();
+        md.setLooping(true);
     }
 }

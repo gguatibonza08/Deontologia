c76f336171e8a0b1db82e0120b2edb3a18b6ba01
Gian Carlos Guatibonza Suarez <gguatibonza08@gmail.com>
2018-04-11 01:50:34 -0500

    se configuro para la seleccion de personaje

--- app/src/main/java/co/com/gguatibonza/deontologia/EleccionAvatar.java
+++ app/src/main/java/co/com/gguatibonza/deontologia/EleccionAvatar.java
@@ -1,13 +1,58 @@
 package co.com.gguatibonza.deontologia;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class EleccionAvatar extends AppCompatActivity {
+public class EleccionAvatar extends AppCompatActivity implements View.OnClickListener {
+
+    private CardView gian, nandy, leo, kolarte;
+    private TextView seleccion;
+    private LinearLayout textos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eleccion_avatar);
+
+        gian = findViewById(R.id.gian);
+        leo = findViewById(R.id.leo);
+        kolarte = findViewById(R.id.kolarte);
+        nandy = findViewById(R.id.nandy);
+        seleccion = findViewById(R.id.seleccion);
+        textos = findViewById(R.id.textos);
+
+
+        gian.setOnClickListener(this);
+        leo.setOnClickListener(this);
+        kolarte.setOnClickListener(this);
+        nandy.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.gian:
+                seleccion.setText(getResources().getString(R.string.gk));
+
+                break;
+            case R.id.nandy:
+                seleccion.setText(getResources().getString(R.string.nandy));
+                break;
+            case R.id.leo:
+                seleccion.setText(getResources().getString(R.string.leo));
+                break;
+            case R.id.kolarte:
+                seleccion.setText(getResources().getString(R.string.kolarte));
+                break;
+
+
+        }
+        textos.setVisibility(View.VISIBLE);
+
     }
 }

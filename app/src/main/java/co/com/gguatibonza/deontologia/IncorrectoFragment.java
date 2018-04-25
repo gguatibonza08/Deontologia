b0267bbe86765df8a35ddfcc9438a5634f6ceee8
luisachacon <lchacon565@unab.edu.co>
2018-04-25 09:23:40 -0500

    imagen y explicacion

--- app/src/main/java/co/com/gguatibonza/deontologia/IncorrectoFragment.java
+++ app/src/main/java/co/com/gguatibonza/deontologia/IncorrectoFragment.java
@@ -2,10 +2,15 @@ package co.com.gguatibonza.deontologia;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 
 /**
@@ -13,17 +18,39 @@ import android.view.ViewGroup;
  */
 public class IncorrectoFragment extends Fragment {
 
+    private TextView explicacion;
+    private ImageView imagenIncorrect;
+    private String explication,imagen;
 
     public IncorrectoFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            explication = bundle.getString("explicacion");
+            imagen=bundle.getString("imagenInCorrecta");
+        }
+        Picasso.get().load("imagenIncorrecta").into(imagenIncorrect);
+        explicacion.setText(explication);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_incorrecto, container, false);
+
+        explicacion=view.findViewById(R.id.traerExplicacion);
+        imagenIncorrect=view.findViewById(R.id.imagenIncorrecta);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_incorrecto, container, false);
+        return  view;
+
+
+
     }
 
 }
